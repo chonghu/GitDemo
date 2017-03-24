e6d86985e94ad1b00ce9425fae1cace89b2895e5
胡冲 <cf_hc1980@hotmail.com>
2017-03-24 23:17:19 +0800

    Hello World!

---
+++ GitDemo/src/com/fox/demo/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.fox.demo;
+
+/**
+ * @author Richard
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("HelloWorld");
+	}
+
+}
